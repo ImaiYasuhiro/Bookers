7e255f9ef8f513e043cc05b9b03b4999804ed494
mottypack0524 <mottypack0524@gmail.com>
2019-01-23 19:22:02 +0900

    [Start]Bookers2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5276b25529452c3e7071e2b9341bddb31722136e4b0cc7ab770d990b19d36eec92b16c402e6edfca61a454deee4d95432245175f7f1a710d9336252d2d6929f0'
--- db/migrate/20190108054527_add_detail_to_bookers.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailToBookers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :bookers, :title, :string
-    add_column :bookers, :category, :string
-    add_column :bookers, :body, :string
-  end
-end
